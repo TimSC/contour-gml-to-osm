b0e036e2a35fcf3fdc31070192636d904a649db4
TimSC <tim2009@sheerman-chase.org.uk>
2013-10-12 23:02:01 +0100

    Read geometry into shapely

--- gml.py
+++ gml.py
@@ -1,13 +1,37 @@
 
 import zipfile, os
 import xml.etree.ElementTree as ET
+from ostn02python import OSTN02, OSGB
+from shapely.geometry import LineString, Point
 
 class ReadGml(object):
 
 	def ReadGeometry(self, geo):
 		for ch in geo:
-			print ch.tag
-		#pass
+			if ch.tag == "{http://www.opengis.net/gml/3.2}LineString":
+				posListEl = ch.find('{http://www.opengis.net/gml/3.2}posList')
+				posFloats = map(float, posListEl.text.split(" "))
+				est = posFloats[0::2] 
+				nth = posFloats[1::2]
+				lonLat = []
+				for e, n in zip(est, nth):
+					(x,y,h) = OSTN02.OSGB36_to_ETRS89 (e, n)
+					(gla, glo) = OSGB.grid_to_ll(x, y)
+					#print e, n, gla, glo, h
+					lonLat.append((glo, gla))
+				
+				shp = LineString(lonLat)
+				#print shp
+
+			if ch.tag == "{http://www.opengis.net/gml/3.2}Point":
+				posEl = ch.find("{http://www.opengis.net/gml/3.2}pos")
+				posFloat = map(float, posEl.text.split(" "))
+				(x,y,h) = OSTN02.OSGB36_to_ETRS89 (posFloat[0], posFloat[1])
+				(gla, glo) = OSGB.grid_to_ll(x, y)
+				
+				shp = Point(glo, gla)
+				#print shp
+			#print ch.tag
 		
 
 	def ReadSpotHeight(self, el):
