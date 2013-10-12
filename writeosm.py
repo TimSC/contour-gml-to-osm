bb4369bc54248f508dc1916c0de283d5b86f525f
TimSC <tim2009@sheerman-chase.org.uk>
2013-10-12 23:59:25 +0100

    Write ways

--- writeosm.py
+++ writeosm.py
@@ -15,19 +15,30 @@ class OsmOutput(object):
 		self.fi.write(u"</osm>\n")
 		self.fi.close()
 
-	def WriteNode(self, objId, lat, lon, keys):
+	def WriteNode(self, objId, lat, lon, tags):
 		self.fi.write(u"<node id='{0}' lat='{1}' lon='{2}'".format(int(objId), float(lat), float(lon)))
-		if len(keys) == 0:
-			self.fi.write(" />\n")
+		if len(tags) == 0:
+			self.fi.write(u" />\n")
 		else:
-			self.fi.write(">\n")
-			for key in keys:
-				self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(keys[key]))))
+			self.fi.write(u">\n")
+			for key in tags:
+				self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))))
 			self.fi.write(u"</node>\n")
+
+	def WriteWay(self, wayId, nodeIds, tags):
+		self.fi.write(u"<way id='{0}'>\n".format(int(wayId)))
+		for nid in nodeIds:
+			self.fi.write(u" <nd ref='{0}' />\n".format(int(nid)))
+		for key in tags:
+			self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))))
+		self.fi.write(u"</way>\n")
+
+
 
 if __name__ == "__main__":
 	
 	osmOutput = OsmOutput("test.osm")
 	osmOutput.WriteNode(-1, 51., -1., {'test':"\"'", 'test2':u"This has ♭"})
 	osmOutput.WriteNode(-2, 50., -2., {})
+	osmOutput.WriteWay(-1, [-1, -2], {"test3": "stuff"})
 
