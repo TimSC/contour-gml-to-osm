62b03c4694a77d34c3c35a4e0acd659e91f51d9a
TimSC <tim2009@sheerman-chase.org.uk>
2013-10-13 10:45:36 +0100

    Handle both file handles and file names

--- convert.py
+++ convert.py
@@ -3,8 +3,9 @@ import gml, writeosm
 if __name__ == "__main__":
 	
 	archName = "/home/tim/Downloads/terr50_cgml_gb/data/so/so00_OST50CONT_20130612.zip"
-	gmlData = gml.ReadGmlZip(archName)
+	#gmlData = gml.ReadGmlZip(archName)
 	
-	print "GML files in archive", len(gmlData)
+	#print "GML files in archive", len(gmlData)
 
+	out = writeosm.OsmOutput("test.osm")
 	
--- writeosm.py
+++ writeosm.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import codecs
 from xml.sax.saxutils import escape, quoteattr
 
 class OsmOutput(object):
 	def __init__(self, fina):
 
-		self.fi = codecs.open(fina, "w", "utf-8")
-		self.fi.write(u"<?xml version='1.0' encoding='UTF-8'?>\n")
-		self.fi.write(u"<osm version='0.6' generator='writeosm.py'>\n")
+		if hasattr(fina, 'write'):
+			self.fi = fina
+		else:
+			self.fi = open(fina, "wt")
+
+		self.fi.write(u"<?xml version='1.0' encoding='UTF-8'?>\n".encode('UTF-8'))
+		self.fi.write(u"<osm version='0.6' generator='writeosm.py'>\n".encode('UTF-8'))
 
 	def __del__(self):
-		self.fi.write(u"</osm>\n")
+		self.fi.write(u"</osm>\n".encode('UTF-8'))
 		self.fi.close()
 
 	def WriteNode(self, objId, lat, lon, tags):
-		self.fi.write(u"<node id='{0}' lat='{1}' lon='{2}'".format(int(objId), float(lat), float(lon)))
+		self.fi.write(u"<node id='{0}' lat='{1}' lon='{2}'".format(int(objId), float(lat), float(lon)).encode('UTF-8'))
 		if len(tags) == 0:
-			self.fi.write(u" />\n")
+			self.fi.write(u" />\n".encode('UTF-8'))
 		else:
-			self.fi.write(u">\n")
+			self.fi.write(u">\n".encode('UTF-8'))
 			for key in tags:
-				self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))))
-			self.fi.write(u"</node>\n")
+				self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))).encode('UTF-8'))
+			self.fi.write(u"</node>\n".encode('UTF-8'))
 
 	def WriteWay(self, wayId, nodeIds, tags):
-		self.fi.write(u"<way id='{0}'>\n".format(int(wayId)))
+		self.fi.write(u"<way id='{0}'>\n".format(int(wayId)).encode('UTF-8'))
 		for nid in nodeIds:
-			self.fi.write(u" <nd ref='{0}' />\n".format(int(nid)))
+			self.fi.write(u" <nd ref='{0}' />\n".format(int(nid)).encode('UTF-8'))
 		for key in tags:
-			self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))))
-		self.fi.write(u"</way>\n")
+			self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))).encode('UTF-8'))
+		self.fi.write(u"</way>\n".encode('UTF-8'))
 
 	def WriteRelation(self, wayId, members, tags):
-		self.fi.write(u"<relation id='{0}'>\n".format(int(wayId)))
+		self.fi.write(u"<relation id='{0}'>\n".format(int(wayId)).encode('UTF-8'))
 		for member in members:
 			self.fi.write(u" <member type={0} ref='{1}' role={2}/>\n".format(
 				quoteattr(escape(member[0])), 
 				int(member[1]), 
 				quoteattr(escape(member[2]))
-				))
+				).encode('UTF-8'))
 		for key in tags:
-			self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))))
-		self.fi.write(u"</relation>\n")
+			self.fi.write(u' <tag k={0} v={1} />\n'.format(quoteattr(escape(key)), quoteattr(escape(tags[key]))).encode('UTF-8'))
+		self.fi.write(u"</relation>\n".encode('UTF-8'))
 
 if __name__ == "__main__":
 	
