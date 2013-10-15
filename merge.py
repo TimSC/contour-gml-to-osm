514916e592e976cd17921cf8f08052911a0c0484
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-10-15 20:15:21 +0100

    Renumber objects to make valid result

--- merge.py
+++ merge.py
@@ -2,18 +2,35 @@ import bz2, os
 import xml.etree.ElementTree as ET
 from ostn02python import OSGB, OSTN02
 
-def MergeFile(fina, ty, existing, fiOut):
+def MergeFile(fina, ty, nextObjId, idMapping, fiOut):
 	fi = bz2.BZ2File(fina)
 	root = ET.fromstring(fi.read())
 
+	if fina not in idMapping:
+		idMapping[fina] = {}
+	fiMapping = idMapping[fina]
+
+	if ty not in fiMapping:
+		fiMapping[ty] = {}
+	tyMapping = fiMapping[ty]
+	ndMapping = fiMapping['node']
+
 	for el in root:
 		if el.tag != ty: continue
-		objId = int(el.attrib['id'])
-		if objId in existing: 
-			print "skipping", el.tag, objId
-			continue
-		print el.tag, objId
-		existing.add(objId)
+		oldId = el.attrib['id']
+		tyMapping[oldId] = str(nextObjId[ty])
+		nextObjId[ty] -= 1
+
+		el.attrib['id'] = tyMapping[oldId]
+		print el.tag, el.attrib['id']
+
+		for ch in el:
+			if ch.tag=="nd":
+				#print ch.tag, ch.attrib
+				#print "Remap node", ch.attrib['ref'], "to", ndMapping[ch.attrib['ref']]
+				ch.attrib['ref'] = ndMapping[ch.attrib['ref']]
+			if ch.tag=="member":
+				assert(0)#Not implemented
 
 		xml = ET.tostring(el, encoding="utf-8")
 		fiOut.write(xml)
@@ -53,30 +70,27 @@ if __name__=="__main__":
 	print "e", minE, maxE
 	print "n", minN, maxN
 
-
+	fiList = []
 	for e in range(minE, maxE+10000, 10000):
 		for n in range(minN, maxN+10000, 10000):
-			print e, n, OSGB.grid_to_os_streetview_tile((e, n))
-
-	exit(0)
-	
-	fiList = []
-	for x in range(xtile1, xtile2+1):
-		for y in range(ytile2, ytile1+1):
-			rootPath = "existing/12"
-			dirName = rootPath+"/"+str(x)
-			fina = dirName+"/"+str(y)+".osm.bz2"
-			if os.path.isfile(fina):
-				fiList.append(fina)
+			tileName = OSGB.grid_to_os_streetview_tile((e, n))
+			print e, n, tileName
+			tileCode = tileName[0].lower()
+			fina = "out/"+str(tileCode[:2])+"/"+str(tileCode[:4])+"_OST50CONT_20130612.osm.bz2"
+			if not os.path.exists(fina): continue
+			fiList.append(fina)
+			
+	nextObjId = {"node": -1, "way": -1, "relation": -1}
+	idMapping = {}
 
 	for fina in fiList:
-		MergeFile(fina, "node", existing, out)
+		MergeFile(fina, "node", nextObjId, idMapping, out)
 
 	for fina in fiList:
-		MergeFile(fina, "way", existing, out)
+		MergeFile(fina, "way", nextObjId, idMapping, out)
 
 	for fina in fiList:
-		MergeFile(fina, "relation", existing, out)
+		MergeFile(fina, "relation", nextObjId, idMapping, out)
 
 	out.write("</osm>\n")
 	out.close()
